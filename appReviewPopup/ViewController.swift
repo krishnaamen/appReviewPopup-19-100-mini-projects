e913cc89a66688d38fc44608db90971afe429ff3
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-05 20:58:21 +0200

    first commit

--- appReviewPopup/ViewController.swift
+++ appReviewPopup/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reviewBtnPressed(_ sender: UIButton) {
+        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                            SKStoreReviewController.requestReview(in: scene)
+                        }
+    }
+    
 }
 
